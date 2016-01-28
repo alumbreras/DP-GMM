5be2a3d3e38f2e4b7b7093895f3c24873353576d
Alberto Lumbreras <alberto.lumbreras@technicolor.com>
2016-01-28 16:35:01 +0100

    first working version\!

--- src/conditionals.r
+++ src/conditionals.r
@@ -1,38 +1,116 @@
 # Sampling functions
 #
 library(MASS)
+library(mvtnorm)
 #source('ars_alpha.r')
 #source('ars_beta.r')
-
-sample_z <-function(A, mu_a0, R_a0){
-  #
-  # Chinese Restaurant Process with auxiliary tables (Neal's algorithm 8)
-  #
-  n <- dim(A)[2]
-  D <- dim(A)[1]
-  R_a0_inv <- solve(R_a0)
-  W_a0_inv <- solve(W_a0)
-  
-  # Create m auxiliary tables from the base distribution
-  m <- 3
-  for(i in 1:m){
+DEBUG <- FALSE
+sample_z <- function(u, A, alpha, z, mu_ar, S_ar, mu_a0, R_a0, beta_a0, W_a0){
+    #
+    # Chinese Restaurant Process with auxiliary tables (Neal's algorithm 8)
+    # 
+    m <- 3 # number of auxiliary tables to approximate the infinite number of empty tables
+    n <- table(z)
+    K <- length(unique(z))
+    D <- dim(A)[1]
+    R_a0_inv <- solve(R_a0)
+    W_a0_inv <- solve(W_a0)
+    
+    # Create m auxiliary tables from the base distribution
     aux.tables.mean <- t(mvrnorm(m, mu_a0, R_a0_inv))
-    aux.tables.covariance <-  rWishart(m, max(beta_a0,D), W_a0_inv/beta_a0)
-  }
+    aux.tables.covariance <-  rWishart(m, max(beta_a0,D), W_a0/beta_a0) #debug: remove the 0.1
   
-  # Compute likelihood for every table
-  # remove first the user assignment
-  # TODO: continue here
-  for(i in 1:K){
-    logp = np.log(self.n[i]-1)
-    logp += self.likelihood_a(u)
-    logprobs.append(logp) 
-  }
-  for(i in 1:m){
-    logp = np.log(self.n[i]-1)/m
-    logp += self.likelihood_a(u)
-    logprobs.append(logp) 
-  }
+    # Compute likelihood for every table
+    logprobs <- rep(NA, K+m)
+    for(k in 1:K){
+      logp <- log(n[k]-as.numeric(z[u]==k))
+      logp <- logp + mvtnorm::dmvnorm(A[,u], mean=mu_ar[,k], sigma=solve(S_ar[,,k]), log=TRUE)
+      logprobs[k] <- logp 
+    }
+    for(k in 1:m){
+      logp <- log(alpha/m)
+      logp <- logp + mvtnorm::dmvnorm(A[,u], mean=aux.tables.mean[,k], sigma=aux.tables.covariance[,,k], log=TRUE)
+      logprobs[K+k] <- logp 
+    }
+    
+    # normalize probabilities to avoid numerical underflow
+    probs <- logprobs-max(logprobs)
+    #cat("\nlogprobs:", probs)
+    probs <- exp(probs)
+    probs <- probs/sum(probs)
+    #cat("\nprobs:", probs)
+    
+    # Choose table
+    chosen <- base::sample(1:(K+m), 1, prob=probs)
+    
+    # Debug:
+    # draw choices and final choice
+    if(DEBUG){
+      par(mfrow=c(1,1))
+      size <- rep(1, dim(A)[2])
+      pch <- rep(1, dim(A)[2])
+      size[u] <- 5
+      pch[u] <- 19
+      plot(t(A), col=palette()[z+1],  cex=size, pch=pch, main=paste("Choosing...", u))
+      ellipse(mu=mu_a0, sigma=W_a0, alpha = .5, lwd=5, npoints = 250, col="black")
+      for(k in 1:K){
+        ellipse(mu=mu_ar[,k], sigma=solve(S_ar[,,k]), alpha=0.5, lwd=3, npoints = 250, col=palette()[k+1])
+        text(mu_ar[1,k],mu_ar[2,k], probs[k])
+      }
+      for(k in 1:m){
+        ellipse(mu=aux.tables.mean[,k], sigma=aux.tables.covariance[,,k], alpha=0.5, lwd=3, lty=2, npoints = 250, col=palette()[k+1])
+        text(aux.tables.mean[1,k],aux.tables.mean[2,k], probs[K+m])
+      }
+      readline(paste(u, "...", chosen))
+    }
+    
+    
+    # Re-label tables
+    ##########################################
+    # if auxiliary table is chosen, assign label K+i-th to this new table
+    if(chosen>K){
+      mu_ar[,K+1] <- aux.tables.mean[, chosen - K]
+      S_ar[,,K+1] <- solve(aux.tables.covariance[,, chosen - K])
+      z[u] <- K+1
+      
+      if(DEBUG){
+        # debugu: current
+        par(mfrow=c(1,2))
+        plot(t(A), col=palette()[z+1], main=paste("Before"))
+        for(k in 1:K){
+          ellipse(mu=mu_a0, sigma=W_a0, alpha = .5, lwd=5, npoints = 250, col="black")
+          ellipse(mu=mu_ar[,k], sigma=solve(S_ar[,,k]), alpha=0.5, lwd=3, npoints = 250, col=palette()[k+1])
+        }
+        
+        #debug: new table!!
+        size <- rep(1, dim(A)[2])
+        pch <- rep(1, dim(A)[2])
+        size[u] <- 5
+        pch[u] <- 19
+        plot(t(A), col=palette()[z], cex=size, pch=pch, main="After (new table!)")
+        for(k in 1:K){
+          ellipse(mu=mu_a0, sigma=W_a0, alpha = .5, lwd=5, npoints = 250, col="black")
+          ellipse(mu=mu_ar[,k], sigma=solve(S_ar[,,k]), alpha = .5, lwd=3, npoints = 250, col=palette()[k+1])
+        }
+        # new table
+        ellipse(mu=mu_ar[,K+1], sigma=solve(S_ar[,,K+1]), npoints = 250, lwd=3, lty=2, col=palette())[K+1+1]  
+      }
+    }
+    else{
+      z[u] <- chosen
+    }
+    
+    # if old cluster is empty, shift left tables to the right
+    if(any(n==0)){
+      empty <- which(n==0)
+      right <- (empty+1):(K+1)
+      mu_ar[,(empty:K)] <- mu_ar[,(empty+1):(K+1)]
+      S_ar[,,(empty:K)] <- S_ar[,,(empty+1):(K+1)]
+      z[z>empty] <- z[z>empty] - 1
+    }
+    return(list(z=z,
+                mu_ar=mu_ar,
+                S_ar=S_ar))
 }
 
 sample_alpha <- function(){
@@ -64,7 +142,7 @@ sample_mu_ar <- function(A_r, S_ar, mu_a0, R_a0){
     return(mvrnorm(1, mu_post, Sigma_post))
 }
 
-sample_S_ar <- function(A_r, beta_a0, W_a0){
+sample_S_ar <- function(A_r, mu_ar_k, beta_a0, W_a0){
     #
     # Sample attributes covariance matrix of cluster r
     # from its conditional probability
@@ -80,9 +158,15 @@ sample_S_ar <- function(A_r, beta_a0, W_a0){
             df <- max(beta_a0, D) 
             return(rWishart(1, df, solve(W_a0)/beta_a0))
     }
+    # If only one point, scatter is just this product
+    if(n==1){
+      scatter_matrix <- (A_r-mu_ar_k)%*%t(A_r-mu_ar_k)
+    }
+    if(n > 1){
+      scatter_matrix <- cov(t(A_r-c(mu_ar_k)))*(dim(A_r)[2]-1)
+    }
     
-    scatter_matrix <- cov(t(A_r-rowMeans(A_r)))*(dim(A_r)[2]-1)
-    wishart_dof_post <- beta_a0 + n    
+    wishart_dof_post <- beta_a0 + n
     wishart_S_post <- solve(beta_a0*W_a0 + scatter_matrix)
     return(rWishart(1, wishart_dof_post, wishart_S_post)[,,1])
 }       
@@ -130,7 +214,12 @@ sample_W_a0 <- function(Lambda_a, S_ar, beta_a0){
     # 
     K <- dim(S_ar)[3]
     D <- dim(S_ar)[1]
+    # TODO: why apply does not work properly?
     scatter_matrix <- apply(S_ar, 3, sum)
+    scatter_matrix <- 0
+    for (i in 1:dim(S_ar)[3]){
+      scatter_matrix <- scatter_matrix + S_ar[,,i]
+    }
     wishart_dof_post = D + K*beta_a0
     wishart_S_post = solve(D * Lambda_a + beta_a0*scatter_matrix)
     return(rWishart(1, wishart_dof_post, wishart_S_post)[,,1])
@@ -144,6 +233,6 @@ sample_beta_a0 <- function(z, S_ar, W_a0, init=beta_init){
     # https://code.google.com/p/pmtk3/source/browse/trunk/toolbox/Algorithms/mcmc/ars.m?r=2678
     
     K <- length(unique(z))
-    S_ar = self.S_ar[,,1:K]         
+    #S_ar = self.S_ar[,,1:K]         
     return(ars_sample_beta_a0(S=S_ar, W=W_a0, init=beta_init))
 }
--- src/gibbs.r
+++ src/gibbs.r
@@ -1,14 +1,32 @@
 # Main file
 # Control the calls to the conditional samplers and store the traces
 library(abind)
+library(coda)
+library(mixtools)
 source('conditionals.r')
-abind(Sigma_a, along = 3)
-sample <- function(A, iters){
-  
+set.seed(3)
+
+data(iris)
+data(geyser)
+
+#res <- sample(t(iris[,1:4]), 1000)
+
+#sample <- function(A, iters){
+#   
+  iters <- 100
   A <- t(iris[,1:4])
-  D <- dim(A)[1]
+  A <- t(geyser)
   
-  # init traces
+  #A <-iris[,3, drop=FALSE]
+  D <- dim(A)[1]
+  N <- dim(A)[2]
+  
+  # traces
+  traces.z <- matrix(NA, iters, N)
+  traces <- matrix(NA, iters, 4)
+  colnames(traces) <- c("n_clusters", "mean_mu_a0", "det(R_a0)", "det(W_a0)")
+  
+  # data mean and covariance
   Sigma_a <- cov(t(A))
   Lambda_a <- solve(Sigma_a)
   mu_a <- rowMeans(A)
@@ -16,28 +34,37 @@ sample <- function(A, iters){
   # init state
   alpha = 2
   z <- rep(1, dim(A)[2])
+  #z <- as.numeric(factor(iris$Species))
+  
   mu_a0 <- mu_a
   R_a0 <- solve(Sigma_a)
   W_a0 <- Sigma_a
   beta_a0 <- dim(A)[1]
-  
-  
-  #S_ar <- replicate(100, Lambda_a)
-  #mu_ar <- replicate(100, mu_a0)
   S_ar <- array(NA, dim=c(D, D, 100))
   mu_ar <- matrix(NA, D, 100)
+  for (k in 1:length(unique(z))){
+    mu_ar[,k] <- rowMeans(A[,z==k])
+  }
+  
   for(i in 1:iters){
     cat('\n', i)
     # Active components
     K <- length(unique(z))
     
+    
     # Sample component parameters
     for (k in 1:length(unique(z))){
       mask <- (z==k)
-      S_ar[,,k] <- sample_S_ar(A[,mask], beta_a0, W_a0)
-      mu_ar[,k] <- sample_mu_ar(A[,mask], S_ar[,,k], mu_a0, R_a0)
+      S_ar[,,k] <- sample_S_ar(A[,mask, drop=FALSE], mu_ar[,k, drop=FALSE], beta_a0, W_a0)
+      mu_ar[,k] <- sample_mu_ar(A[,mask, drop=FALSE], S_ar[,,k], mu_a0, R_a0)
     }
-
+    par(mfrow=c(1,2))
+    plot(t(A), col=palette()[z+1], main=paste("Updated components"))
+    for(k in 1:K){
+      ellipse(mu=mu_a0, sigma=W_a0, alpha = .5, lwd=5, npoints = 250, col="black")
+      ellipse(mu=mu_ar[,k], sigma=solve(S_ar[,,k]),  alpha = .5, lwd=3, npoints = 250, col=palette()[k+1])
+    }
+    
     # Sample hyperpriors
     mu_a0 <- sample_mu_a0(Lambda_a, mu_a, mu_ar[,1:K, drop=FALSE], R_a0)
     R_a0 <- sample_R_a0(Sigma_a, mu_ar[,1:K, drop=FALSE], mu_a0) 
@@ -45,13 +72,24 @@ sample <- function(A, iters){
     #beta_a0 <- ars_sample_beta_a0(S=S_ar, W=W_a0, init=beta_a0)
     
     # Sample assignments and concentration parameter
-    #z <- sample_z()
+    for (n in 1:N){
+      res <- sample_z(n, A, alpha, z, mu_ar, S_ar, mu_a0, R_a0,  beta_a0, W_a0)
+      z <- res$z
+      mu_ar <- res$mu_ar
+      S_ar <- res$S_ar
+    }
+    K <- length(unique(z))
+    cat('\n', table(z))
     #alpha <- ars_sample_alpha(K = self.R, U = self.U)
-    
-    
+    print(mu_ar[,1:K])
+    traces[i,] <- c(length(unique(z)), mean(mu_a0), det(R_a0), det(W_a0))
+    traces.z[i,] <- z
   }
-}
+#  return(list(traces=traces,
+#              traces.z=traces.z))
+#}
 
-# Main #
-data(iris)
-sample(t(iris[,1:4]), 1000)
+#traces <- res$traces
+#traces.z <- res$traces.z
+#chains <- mcmc(traces)
+#plot(chains)
